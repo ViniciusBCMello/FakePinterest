0b607eafb7d46f8386e8ca854125ac3b1d4c0c20
ViniciusBCMello <viniciusbcmello@gmail.com>
2024-08-08 00:18:45 -0300

    primeiro commit

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -3,7 +3,9 @@ from  flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
